8ab913dd51c303bf666ad0fe0af01ee6d0755c83
driftboat <zhengshu1.1@163.com>
2023-07-06 10:27:47 +0800

    add_memory_with_importance

--- game_simulation/test_agent.py
+++ game_simulation/test_agent.py
@@ -13,7 +13,7 @@ class TestAgent(unittest.TestCase):
         description = "John Lin is a pharmacy shopkeeper at the Willow Market and Pharmacy who loves to help people. He is always looking for ways to make the process of getting medication easier for his customers; John Lin is living with his wife, Mei Lin, who is a college professor, and son, Eddy Lin, who is a student studying music theory; John Lin loves his family very much; John Lin has known the old couple next-door, Sam Moore and Jennifer Moore, for a few years; John Lin thinks Sam Moore is a kind and nice man; John Lin knows his neighbor, Yuriko Yamamoto, well; John Lin knows of his neighbors, Tamara Taylor and Carmen Ortiz, but has not met them before; John Lin and Tom Moreno are colleagues at The Willows Market and Pharmacy; John Lin and Tom Moreno are friends and like to discuss local politics together; John Lin knows the Moreno family somewhat well — the husband Tom Moreno and the wife Jane Moreno"
         des_arr = description.split(";")
         for des in des_arr: 
-            agent.add_memory(now,des, MemoryType.OBSERVATION,prompt_meta)
+            agent.add_memory_with_importance(now,des, MemoryType.OBSERVATION,10,prompt_meta)
         agent.gen_summary(prompt_meta)
         print(agent.summary)
         self.assertIsNotNone(agent.summary)
